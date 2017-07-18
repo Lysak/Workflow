64cd38fc1e2a99ba86cd2d45b3ce0533536eff4e
Dmytrii Lysak <dlysak@live.com>
2017-07-18 22:24:31 +0300

    next stage, dev first component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import NoteEditor from './NoteEditor.jsx';
+import NotesGrid from './NotesGrid.jsx';
+import Infographic from './Infographic.jsx';
+
+import './App.less';
+
 class App extends React.Component{
+    handleNoteAdd(data) {
+        console.log(data);
+    }
+
     render() {
-        return <h1> Workflow </h1>;
+        return (
+            <div className='App'>
+                <h2 className='App__header'>Workflow</h2>
+                <NoteEditor onNoteAdd={this.handleNoteAdd} />
+                <NotesGrid />
+                {/* notes={this.state.notes} onNoteDelete={this.handleNoteDelete} */}
+                <Infographic />
+            </div>
+        );
     }
 };
 
---
+++ client/components/Infographic.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Infographic extends React.Component{
+    render() {
+        return <h1> Infographic </h1>;
+    }
+};
+
+export default Infographic;
